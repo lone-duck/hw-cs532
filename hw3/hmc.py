8b6e3a5faa12cb8acc642ec06b307e754dafc226
Dylan Green <dyyylan@yup.local>
2021-10-25 20:46:42 -0700

    HMC working

--- hw3/hmc.py
+++ hw3/hmc.py
@@ -1,10 +1,148 @@
-# metropolis hastings within gibbs
+# hamiltonian_monte_carlo
+# TODO: clean up ENV behaviour so that all this sample_from_graph, jll, deterministic_eval
+# can be imported
 
 import torch 
 from primitives import eval_env
 from graph_utils import topological_sort
+import copy
 
 ENV = eval_env()
+
+
+def hmc(graph, S, X0=None, T=20, epsilon=0.1, m=1, return_samples=False, report_acceptance_rate=False):
+    # NOTE: for now, assume that len(X) is dimensionality of R
+    # this may not be the case if element of X are vectors themselves
+    
+    if return_samples:
+        samples = [None]*S
+    if report_acceptance_rate:
+        accepts = 0
+    
+    jlls = [None]*S
+    ret_vals = [None]*S
+
+    if X0 is None:
+        X0 = sample_from_prior(graph)
+
+    Xprev = X0
+    Xdim = len(X0)
+
+    ret_expr = graph[2]
+
+    R_dist = torch.distributions.MultivariateNormal(torch.zeros(Xdim), m*torch.eye(Xdim))
+    unif = torch.distributions.uniform.Uniform(0, 1)
+
+    for s in range(S):
+        # get a new R
+        R_vals = R_dist.sample()
+        R = dict(zip(Xprev.keys(), R_vals))
+        Xprime, Rprime = leapfrog(graph, copy.deepcopy(Xprev), copy.deepcopy(R), T, epsilon)
+        # compute acceptance values
+        u = unif.sample()
+        alpha = accept(graph, Xprev, R, Xprime, Rprime, m)
+        # based on acceptance values, choose which X to keep
+        if u < alpha:
+            if report_acceptance_rate:
+                accepts +=1
+            Xkeep = Xprime
+        else:
+            Xkeep = Xprev
+
+        # store appropriate things
+        if return_samples:
+            samples[s] = Xkeep
+        ret_vals[s] = deterministic_evaluate(ret_expr, Xkeep)[0]
+        jlls[s] = joint_log_likelihood(Xkeep, graph)
+
+        # update Xprev
+        Xprev = Xkeep
+
+    if report_acceptance_rate:
+        print("Acceptance rate: {}".format(accepts/S))
+
+    if return_samples:
+        return ret_vals, jlls, samples
+    else:
+        return ret_vals, jlls
+
+
+def leapfrog(graph, X, R, T, epsilon):
+    gradU = get_grad_U(graph, X)
+    R = R_update(R, gradU, epsilon, half_step=True)
+
+    for t in range(T-1):
+        X = X_update(X, R, epsilon)
+        gradU = get_grad_U(graph, X)
+        R = R_update(R, gradU, epsilon)
+
+    X = X_update(X, R, epsilon)
+    gradU = get_grad_U(graph, X)
+    R = R_update(R, gradU, epsilon, half_step=True)
+    return X, R
+
+
+def get_grad_U(graph, X):
+
+    P = graph[1]['P']
+    Y = {k: torch.tensor(v).float() for k, v in graph[1]['Y'].items()}
+    V = {**X, **Y}
+    Vkeys = list(V.keys())
+    U = 0
+    for v in Vkeys:
+        V[v].requires_grad=True
+        if V[v].grad != None:
+            V[v].grad.zero_()
+    for v in Vkeys:
+        expr = P[v][1]
+        p_v, _ = deterministic_evaluate(expr, V)
+        U += p_v.log_prob(V[v])
+
+    U *= -1
+    U.backward()
+    gradU = {key: value.grad for key, value in X.items()}
+
+    return gradU
+
+
+def R_update(R, gradU, epsilon, half_step=False):
+    
+    keys = list(R.keys())
+    if half_step:
+        epsilon = 0.5*epsilon
+
+    for r in keys:
+        R[r] -= epsilon*gradU[r]
+
+    return R
+
+
+def X_update(X, R, epsilon):
+    keys = list(X.keys())
+
+    for x in keys:
+        X[x].requires_grad = False
+        X[x] += epsilon*R[x]
+
+    return X
+
+
+def accept(graph, X, R, Xprime, Rprime, m):
+    
+    K = 0
+    for r in R.values():
+        K += 0.5*r**2/m
+    U = -1*joint_log_likelihood(X, graph)
+    H = K + U
+
+    Kprime = 0 
+    for r in Rprime.values():
+        Kprime += 0.5*r**2/m 
+    Uprime = -1*joint_log_likelihood(Xprime, graph)
+    Hprime = Kprime + Uprime
+
+    return torch.exp(-Hprime + H)
+
 
 def sample_from_prior(graph):
     # get contents of graph
@@ -51,81 +189,6 @@ def joint_log_likelihood(X, graph):
         jll += p_v.log_prob(V[v])
 
     return jll
-
-
-
-def gibbs_sampling(graph, S, X0=None, verbose=False, return_samples=False):
-
-    if return_samples:
-        samples = [None]*S
-    
-    jlls = [None]*S
-    ret_vals = [None]*S
-
-    if X0 is None:
-        X0 = sample_from_prior(graph)
-
-    Xprev = X0
-
-    for s in range(S):
-        if verbose:
-            print(s)
-        X, ret_vals[s] = gibbs_step(Xprev, graph)
-        if return_samples:
-            samples[s] = X
-        jlls[s] = joint_log_likelihood(X, graph)
-        Xprev = X
-
-    if return_samples:
-        return ret_vals, jlls, samples
-    else:
-        return ret_vals, jlls
-
-
-def gibbs_step(X, graph):
-
-    P = graph[1]['P']
-    unif = torch.distributions.Uniform(0,1)
-    Xkeys = list(X.keys())
-    ret_vals = graph[2]
-
-    for x in Xkeys:
-        task, expr = P[x][0], P[x][1]
-        assert task == "sample*", "Found observed variable in X???"
-        q, _ = deterministic_evaluate(expr, X)
-        Xprime = X.copy()
-        Xprime[x] = q.sample()
-        alpha = accept(x, Xprime, X, graph)
-        u = unif.sample()
-        if u < alpha:
-            X = Xprime
-
-    return X, deterministic_evaluate(ret_vals, X)[0]
-
-
-def accept(x, Xprime, X, graph):
-    P = graph[1]['P']
-    Y = {k: torch.tensor(v).float() for k, v in graph[1]['Y'].items()}
-    A = graph[1]['A']
-    log_alpha = 0.0
-
-    task, expr = P[x][0], P[x][1]
-    assert task == "sample*", "Found observed variable in X???"
-    q, _ = deterministic_evaluate(expr, X)
-    qprime, _ = deterministic_evaluate(expr, Xprime)
-    log_alpha += (qprime.log_prob(X[x]) - q.log_prob(Xprime[x]))
-    
-    V_x = A[x] + [x]
-    XUY = {**X, **Y}
-    XprimeUY = {**Xprime, **Y}
-    for v in V_x:
-        task, expr = P[v][0], P[v][1]
-        p_v, _ = deterministic_evaluate(expr, XprimeUY)
-        log_alpha += p_v.log_prob(XprimeUY[v])
-        p_v, _ = deterministic_evaluate(expr, XUY)
-        log_alpha -= p_v.log_prob(XUY[v])
-
-    return torch.exp(log_alpha)
 
 
 def deterministic_evaluate(e, l, sig=None):
@@ -187,13 +250,5 @@ def deterministic_evaluate(e, l, sig=None):
             c_is = [deterministic_evaluate(arg, l)[0] for arg in e[1:]]
             l_proc = dict(zip(v_is, c_is))
             return deterministic_evaluate(e0, {**l, **l_proc})
-
-
-
-
-
-
-
-
 
 
