abe726d5801ab5fafab74d2b0471b862f63acc05
Dylan Green <dyyylan@yup.local>
2021-11-10 16:10:46 -0800

    deterministic tests passing

--- hw5/evaluator.py
+++ hw5/evaluator.py
@@ -1,51 +1,109 @@
-from primitives import env as penv
+from primitives import BASE_ENV
 from tests import is_tol, run_prob_test,load_truth
 from pyrsistent import pmap,plist
+import copy
+import json
+import torch
+
+
+def evaluate_outer(e, env=None): #TODO: add sigma, or something
+    
+    # create env if it doesn't exist
+    if env is None:
+        env = standard_env()
+
+    # evaluate, which turns into a single fn
+    proc = evaluate(e, env)
+
+    # call fn with alpha = ''
+    return proc('')
+
+def evaluate(e, env):
+    #evaluate expression
+    #base cases
+    if isinstance(e, (int, float)):
+        return torch.tensor(float(e))
+    elif isinstance(e, str):
+        try:
+            return env.find(e)[e]
+        except:
+            assert False, "Could not find " + e
+    # is there another case?
+    else:
+        assert isinstance(e, list), "Found unexpected base case: {}".format(e)
+
+    # recursive cases
+    op, *args = e
+    if op == 'fn':
+        params = args[0]
+        body = args[1]
+        return Procedure(params, body, copy.deepcopy(env))
+    else:
+        proc = evaluate(op, env)
+        addr = args[0]
+        args = args[1:]
+        vals = [evaluate(arg, env) for arg in args]
+        return proc(*vals)
+
+
+# copied from Lispy
+class Env(dict):
+    "An environment: a dict of {'var': val} pairs, with an outer Env."
+    def __init__(self, params=(), args=(), outer=None):
+        self.update(zip(params, args))
+        self.outer = outer
+    def find(self, var):
+        "Find the innermost Env where var appears."
+        return self if (var in self) else self.outer.find(var)
+
+class Procedure(object):
+    "A user-defined Scheme procedure."
+    def __init__(self, params, body, env):
+        self.params, self.body, self.env = params, body, env
+    def __call__(self, *args): 
+        # todo: make deep copies?
+        return evaluate(self.body, Env(self.params, args, self.env))
+
 
 def standard_env():
-    "An environment with some Scheme standard procedures."
-    env = pmap(penv)
-    env = env.update({'alpha' : ''}) 
+    
+    env = Env()
+    env.update(BASE_ENV)
+    env.update({'alpha' : ''}) 
 
     return env
 
 
-
-def evaluate(exp, env=None): #TODO: add sigma, or something
-
-    if env is None:
-        env = standard_env()
-
-    #TODO:
-    return    
-
-
 def get_stream(exp):
     while True:
-        yield evaluate(exp)
+        yield evaluate_outer(exp)
 
 
 def run_deterministic_tests():
     
     for i in range(1,14):
 
+        print("Starting FOPPL test {}".format(i))
+
         with open("programs/tests/deterministic/test_{}.json".format(str(i)), 'rb') as f:
             exp = json.load(f)
         truth = load_truth('programs/tests/deterministic/test_{}.truth'.format(i))
-        ret = evaluate(exp)
+        ret = evaluate_outer(exp)
         try:
             assert(is_tol(ret, truth))
         except:
             raise AssertionError('return value {} is not equal to truth {} for exp {}'.format(ret,truth,exp))
         
-        print('FOPPL Tests passed')
+        print("FOPPL test {} passed".format(i))    
+        
+    print('FOPPL Tests passed')
         
     for i in range(1,13):
 
         with open("programs/tests/hoppl-deterministic/test_{}.json".format(str(i)), 'rb') as f:
             exp = json.load(f)
         truth = load_truth('programs/tests/hoppl-deterministic/test_{}.truth'.format(i))
-        ret = evaluate(exp)
+        ret = evaluate_outer(exp)
         try:
             assert(is_tol(ret, truth))
         except:
@@ -89,4 +147,4 @@ if __name__ == '__main__':
         with open("programs/{}.json".format(str(i)), 'rb') as f:
             exp = json.load(f)
         print('\n\n\nSample of prior of program {}:'.format(i))
-        print(evaluate(exp))        
+        print(evaluate_outer(exp))        
--- hw5/primitives.py
+++ hw5/primitives.py
@@ -1,5 +1,7 @@
 import torch
 import torch.distributions as dist
+from pyrsistent import pmap,plist
+from copy import deepcopy
 
 
 
@@ -35,16 +37,51 @@ class Normal(dist.Normal):
         return super().log_prob(x)
         
 
+def mat_repmat(t, i1, i2):
+    return t.repeat(i1.long().item(), i2.long().item())
+
 def push_addr(alpha, value):
     return alpha + value
 
+def vector_list_creation(args):
+    try:
+        return torch.stack(args)
+    except:
+        return args
+
+def pure_hashmap_update(d, k, v):
+    if isinstance(k, torch.Tensor):
+        k = k.item()
+    d2 = deepcopy(d)
+    d2.update({k:v})
+    return d2
 
 
-env = {
-           'normal' : Normal,
-           'push-address' : push_addr,
-       }
-
+BASE_ENV = {
+            'push-address' : push_addr,
+            '+': torch.add,
+            '-': torch.sub,
+            '*': torch.mul,
+            '/': torch.div,
+            '>': torch.gt,
+            '<': torch.lt,
+            '>=': torch.ge,
+            '<=': torch.le, 
+            '=': torch.eq,
+            'sqrt': torch.sqrt,
+            'exp': torch.exp,
+            'mat-tanh': torch.tanh,
+            'mat-add': torch.add,
+            'mat-mul': torch.matmul,
+            'mat-repmat': mat_repmat,
+            'vector' : lambda *x: vector_list_creation(x),
+            'get': lambda x, y: deepcopy(x[y.long()]) if isinstance(x, torch.Tensor) else deepcopy(x[y.item() if isinstance(y, torch.Tensor) else y]),
+            'put': lambda x, y, z: deepcopy(torch.cat((x[:y.long()], torch.tensor([z]), x[y.long()+1:]))) if isinstance(x, torch.Tensor) else pure_hashmap_update(x,y,z),
+            'first' : lambda x: deepcopy(x[0]),
+            'last' : lambda x: deepcopy(x[-1]),
+            'append' : lambda x, y: deepcopy(torch.cat((x, torch.tensor([y])))),
+            'hash-map': lambda *x : deepcopy(dict(zip([i.item() if isinstance(i, torch.Tensor) else i for i in x[::2]], x[1::2]))),
+            }
 
 
 
