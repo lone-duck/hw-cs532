d03d35f21f33684ff271eec7c3c47445d3509021
Dylan Green <dyyylan@yup.local>
2021-11-11 22:42:21 -0800

    final update?

--- hw5/primitives.py
+++ hw5/primitives.py
@@ -1,41 +1,7 @@
 import torch
 import torch.distributions as dist
 from pyrsistent import pmap,plist
-from copy import deepcopy
-
-
-
-class Normal(dist.Normal):
-    
-    def __init__(self, alpha, loc, scale):
-        
-        if scale > 20.:
-            self.optim_scale = scale.clone().detach().requires_grad_()
-        else:
-            self.optim_scale = torch.log(torch.exp(scale) - 1).clone().detach().requires_grad_()
-        
-        
-        super().__init__(loc, torch.nn.functional.softplus(self.optim_scale))
-    
-    def Parameters(self):
-        """Return a list of parameters for the distribution"""
-        return [self.loc, self.optim_scale]
-        
-    def make_copy_with_grads(self):
-        """
-        Return a copy  of the distribution, with parameters that require_grad
-        """
-        
-        ps = [p.clone().detach().requires_grad_() for p in self.Parameters()]
-         
-        return Normal(*ps)
-    
-    def log_prob(self, x):
-        
-        self.scale = torch.nn.functional.softplus(self.optim_scale)
-        
-        return super().log_prob(x)
-        
+from copy import deepcopy        
 
 def mat_repmat(t, i1, i2):
     return t.repeat(i1.long().item(), i2.long().item())
